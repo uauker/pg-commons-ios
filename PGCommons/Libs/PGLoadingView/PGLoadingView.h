373a781678fb435b6495a4c0fcf862767c1cf864
Paulo Guilherme <uauker@gmail.com>
2014-02-22 10:17:40 -0300

    Incluido delegate de loading

---
+++ PGCommons/Libs/PGLoadingView/PGLoadingView.h
@@ -0,0 +1,33 @@
+//
+//  PGLoadingView.h
+//  PGCommons
+//
+//  Created by Uauker on 2/22/14.
+//  Copyright (c) 2014 Uauker Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol PGLoadingViewDelegate <NSObject>
+
+@required
+- (UIView *)viewToLoading;
+
+@optional
+- (NSString *)nibToLoading;
+
+@end
+
+
+
+@interface PGLoadingView : UIView
+
+
+@property (nonatomic, weak) id<PGLoadingViewDelegate> delegate;
+
+@property (nonatomic, strong) UIView *view;
+
+
+- (id)initWithDelegate:(id<PGLoadingViewDelegate>) delegate;
+
+@end
---
+++ PGCommons/Libs/PGLoadingView/PGLoadingView.m
@@ -0,0 +1,47 @@
+//
+//  PGLoadingView.m
+//  PGCommons
+//
+//  Created by Uauker on 2/22/14.
+//  Copyright (c) 2014 Uauker Inc. All rights reserved.
+//
+
+#import "PGLoadingView.h"
+
+@implementation PGLoadingView
+
+static NSString * const PGNibName = @"PGLoadingView";
+
+#pragma public
+
+- (id)initWithDelegate:(id<PGLoadingViewDelegate>) delegate {
+    self = [super init];
+    
+    if (self) {
+        self.delegate = delegate;
+        
+        [self initializeView];
+    }
+    
+    return self;
+}
+
+- (void)show {
+    [[self.delegate viewToLoading] addSubview:self.view];
+}
+
+- (void)hide {
+    [self.view removeFromSuperview];
+}
+
+
+#pragma private
+
+- (void)initializeView {
+    NSString *xibName = [self.delegate respondsToSelector:@selector(nibToLoading)] ? [self.delegate nibToLoading] : PGNibName;
+    
+    self.view = [[[NSBundle mainBundle] loadNibNamed:xibName owner:self options:nil] objectAtIndex:0];
+//    [self addSubview:self.view];
+}
+
+@end
